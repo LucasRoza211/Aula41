1c10d210a3965e84b1286c638c91637d49a955e5
lucasroza211 <macorreia211@gmail.com>
2022-05-05 17:56:22 -0300

    commit 1

---
+++ teste1.js
@@ -0,0 +1,12 @@
+var pessoa = {
+    "nome" : "Lucas",
+    "idade" : "19"
+}
+
+console.log("Dados da pessoa: ",pessoa)
+
+var idade = pessoa.idade;
+console.log("Idade da pessoa: ",idade)
+
+pessoa.nome = "José"
+console.log("Novo nome da pessoa: ",pessoa.nome)
---
+++ teste2.js
@@ -0,0 +1,11 @@
+var frutas = '["Uva", "maça", "pera"]'
+var frutasObj = JSON.parse(frutas)
+
+console.log(frutasObj[0])
+
+var populacao = [
+    {"nome":"Joao","idade":25},
+    {"nome":"Jose","idade":23}
+]
+
+console.log(populacao)
